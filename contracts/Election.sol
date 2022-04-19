bc43f3612f6283b4122ddb881e1418e06d87878e
Denedo Pearl <pearliboy1@gmail.com>
2022-04-19 08:49:07 +0100

    few function additions

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,6 +8,7 @@ contract Election {
     address public students;
     address public Teachers;
     address public BoardOfDirectors;
+    address public compilers;
 
 
     event DelegateChairman (address indexed to);
@@ -59,18 +60,22 @@ contract Election {
         require(msg.sender == chairman, "Only the chairman can perform this function");
         _;
     }
+    modifier onlyCompiler() {
+        require(isCompiler() == true, "Only a compiler can perform this function");
+        _;
+    }
     modifier onlyStudent() {
         require(isStudent() == true, "Only students can perform this function");
         _;
     }
-    modifier onlyTeacher() {
+    /*modifier onlyTeacher() {
         require(isTeacher() == true, "Only a teacher can perform this function");
         _;
     }
     modifier onlyBoardMember() {
         require(isBoardMember() == true, "Only a board member can perform this function");
         _;
-    }
+    }*/
     modifier onlyStakeholder() {
         require(stakeHolderExists[msg.sender] == true, "Only a stakeholder do this");
         _;
@@ -88,7 +93,7 @@ contract Election {
     }
 
 
-    //checking if the current address is a board member
+    /*//checking if the current address is a board member
     function isBoardMember() public view returns (bool) {
         if (keccak256(abi.encodePacked(stakeholders[msg.sender].role)) == keccak256(abi.encodePacked("Board Member"))) {
             return true;
@@ -104,7 +109,7 @@ contract Election {
         } else {
             return false;
         }
-    }
+    }*/
 
     //checking if the current address is a student
     function isStudent() public view returns (bool) {
@@ -114,9 +119,25 @@ contract Election {
             return false;
         }
     }
+    //checking if the current address is a teacher or board member
+    function isCompiler() public view returns (bool) {
+        if (keccak256(abi.encodePacked(stakeholders[msg.sender].role)) == keccak256(abi.encodePacked("Teachers")) || (keccak256(abi.encodePacked(stakeholders[msg.sender].role)) == keccak256(abi.encodePacked("BoardOfDirectors")))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /// @notice Check for verifying if an address is a teacher or board member
+    function compilerCheck(address _addr) public view returns (bool) {
+       if (keccak256(abi.encodePacked(stakeholders[msg.sender].role)) == keccak256(abi.encodePacked("Teachers")) || (keccak256(abi.encodePacked(stakeholders[msg.sender].role)) == keccak256(abi.encodePacked("BoardOfDirectors")))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-    /// @notice Check for verifying if an address is a board member
-    function membershipCheck(address _addr) public view returns (bool) {
+   /* /// @notice Check for verifying if an address is a board member
+    function boardMemberCheck(address _addr) public view returns (bool) {
         if (keccak256(abi.encodePacked(stakeholders[_addr].role)) == keccak256(abi.encodePacked("Board Member"))) {
             return true;
         } else {
@@ -131,7 +152,7 @@ contract Election {
         } else {
             return false;
         }
-    }
+    }*/
 
     /// @notice Check for verifying if an address is a student
     function studentshipCheck(address _addr) public view returns (bool) {
@@ -143,10 +164,9 @@ contract Election {
     }
 
 
-
     //Declare interest for leadership position
     function showInterest (string memory _name, string memory _positionContesting) public onlyStakeholder returns(uint){
-        require(isBoardMember() == true || isTeacher() == true || isStudent() == true, "Interest Denied : Unrecognised address, neither board member nor teacher");
+        require(isCompiler() == true, "Interest Denied : Unrecognised address, neither board member nor teacher");
         require(msg.sender != address(0), "invalid address");
         require(isContesting[msg.sender][_positionContesting] == false, "You have already shown interest in this position");
 
@@ -166,7 +186,7 @@ contract Election {
     function delegateChairmanship(address newChairman) public onlyChairman returns(bool changed){
         //require(newChairman == BoardOfDirectors || newChairman == Teachers);
         require(stakeHolderExists[newChairman] == true, "This address is not stakeholder at all");
-        require(membershipCheck(newChairman) == true || teacherCheck(newChairman) == true, "Chairmanship role can't be granted : Not a teacher or board member");
+        require(compilerCheck(newChairman) == true, "Chairmanship role can't be granted : Not a teacher or board member");
         require(studentshipCheck(newChairman) == false, "Chairmanship role can't be granted : Unauthorised address");
         require(newChairman != address(0), "Invalid address");
 
