559267252b5fcb583704570e6f86db54037d0f7e
Maureen Anyanwu <maureenanyanwu519@gmail.com>
2022-04-19 09:30:13 +0100

    added landing page and connect wallet functionality

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,8 +8,8 @@ const Home = () => {
 		setLoading(true);
 	}
     return (
-    	loading === false ? (<Box d="flex" flexDirection={{base:"column", md:"row"}} px={{base:5, md:10}} py={10} mx="auto" justifyContent="space-between">
-	    	    		<Box w={{base:"100%", md:"40%"}} fontSize="4xl" fontWeight="700" mt="50px">
+    	loading === false ? (<Box d="flex" flexDirection={{base:"column-reverse", md:"row"}} px={{base:5, md:10}} py={10} mx="auto" justifyContent="space-between">
+	    	    		<Box w={{base:"100%", md:"40%"}} fontSize={{base:"3xl",md:"2xl",lg:"4xl"}} fontWeight="700" mt="50px">
 	    	    			<Text>
 	    		    			Vote for the future of Zuri with <span style={{color:"yellow"}}>ZuriPolls</span>
 	    		    		</Text>
@@ -17,7 +17,7 @@ const Home = () => {
 	    		    			Your Vote translates to your voice and your right to make a choice	
 	    		    		</Text>
 	    		    		
-	    		    		<Button bg="yellow" px={10} size="lg" mx="auto" color="black" onClick={handlePolls}> Go to Polls</Button>
+	    		    		<Button bg="yellow" isFullWidth mt={3} size="lg" color="black" onClick={handlePolls}> Go to Polls</Button>
 	    	    		</Box>
 	    	    		<Box w={{base:"100%", md:"50%"}} d="flex" justifyContent="flex-end">
 	    	    			<Image src="hero.svg" htmlHeight="80%" htmlWidth="100%"/>	
