5e006492fcc7cd159d4c876d8353f48bd59346f3
Denedo Pearl <pearliboy1@gmail.com>
2022-04-21 17:35:05 +0100

    update some tests

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,12 +1,24 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+
+require("@nomiclabs/hardhat-waffle"); 
+
 describe("Election", function() {
+   let contract;
+   let owner;
+
+  beforeEach(async function () {
+    const Election = await ethers.getContractFactory("Election");
+    const elction = await Election.deploy();
+    contract = await election.deployed();
+    
+    [owner] = await ethers.getSigners();
+  });
+
+
   describe("mint token", function() {  
     it("Should mint", async function() {
-      const Election = await ethers.getContractFactory("Election")
-      const contract = await Election.deploy()
-      await contract.deployed()
 
       const chairman = await contract.chairman()
       const actualBalance = await contract.balanceOf(chairman)
@@ -18,49 +30,35 @@ describe("Election", function() {
       console.log("\tActual balance: ", actualBalance.toBigInt())
 
       expect(expectedBalance).to.equal(actualBalance)
-    })
-  })
+    });
 
-//   describe("swap token", function() {
-//     it("User token should reduce after token swap", async function() {
-//       const NestcoinToken = await ethers.getContractFactory("NestcoinToken")
-//       const nestcoin_token = await NestcoinToken.deploy()
-//       await nestcoin_token.deployed()
+    it("Should confirm the role of a stakeholder is boardmember", async function() {
+      console.log("\n    ✅ confirming...\n");
+      const test = await contract.stakeholders[msg.sender].role;
+      await sleep(5000); // wait 5 seconds for transaction to confirm!!
+      expect(stakeholders[msg.sender].role).to.equal(0);
+    });
 
-//     })
-//   })
+  it("Should confirm the role of a stakeholder is Teacher", async function() {
+    console.log("\n    ✅ confirming...\n");
+    const test = await contract.stakeholders[msg.sender].role;
+    await sleep(5000); // wait 5 seconds for transaction to confirm!!
+    expect(stakeholders[msg.sender].role).to.equal(1);
+  });
 
-//   describe("Token contract", function () {
-//     let contract;
-//     let NestcoinToken;
-//     let owner;
-//     let addr1;
-//     let addrs;
-  
-//     beforeEach(async function () {
-//       [owner, addr1, ...addrs] = await ethers.getSigners(); 
-  
-//       NestcoinToken = await ethers.getContractFactory("BatchTransact")
-//       contract = await NestcoinToken.deploy()
-//     });
-  
-//     describe("Transaction", function () {
+it("Should confirm the role of a stakeholder is student", async function() {
+  console.log("\n    ✅ confirming...\n");
+  const test = await contract.stakeholders[msg.sender].role;
+  await sleep(5000); // wait 5 seconds for transaction to confirm!!
+  expect(stakeholders[msg.sender].role).to.equal(2);
+});
 
-//       it('Should make batch transfer ', async function() {
-//         const BatchTranfer = await NestcoinToken.transfer(addr1, ...addrs);
-//         expect(BatchTranfer).to.increase(200);
-  
-//         //owner to check the remaining token after distribution
-//         const CheckTokenBalance = await NestcoinToken.balanceOf(owner.address);
-//         expect(CheckTokenBalance).sub(totalSupply);
-  
-//         //current user to check their balance
-//         const UserBalance = await NestcoinToken.balanceOf(addr1, ...addrs);
-//         expect(UserBalance).to.increase(200);
-  
-        
-//       })
-//   });
-// })
-// });
+it("Should confirm the role of a stakeholder is compiler", async function() {
+  console.log("\n    ✅ confirming...\n");
+  const test = await contract.stakeholders[msg.sender].role;
+  await sleep(5000); // wait 5 seconds for transaction to confirm!!
+  expect(stakeholders[msg.sender].role).to.equal(0 || 1);
+});
 
+});
+});
